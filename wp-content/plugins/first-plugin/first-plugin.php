666644142a4e3758ec8a4c5ed1f2a8581e8b24e8
melanisulengoikt <melani.sulengo@khk.ee>
2021-03-03 14:29:55 +0200

    modified plugin `first-plugin` version 1.0 (after activation of `Valentine's Day Hearts` version 2.0)

--- wp-content/plugins/first-plugin/first-plugin.php
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -7,3 +7,9 @@
  * Author: Melani Sulengo
  * Author url:http://melanisulengo.ikt.khk.ee/wordpress/
 **/
+
+add_action( 'the_content', 'my_thank_you_text' );
+
+function my_thank_you_text ( $content ) {
+    return $content .= '<p>Thank you for reading!</p>';
+}
