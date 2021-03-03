4fb0c88c1389fea47b4612d09b65caa8fb9473d5
melanisulengoikt <melani.sulengo@khk.ee>
2021-03-03 13:54:21 +0200

    modified plugin `first-plugin` version 1.0 (after activation of `first-plugin` version 1.0)

---
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin name: first-plugin
+ * Plugin url: http://melanisulengo.ikt.khk.ee/wordpress/
+ * Desc : This is the  first plugin I have ever created
+ * Version: 1.0
+ * Author: Melani Sulengo
+ * Author url:http://melanisulengo.ikt.khk.ee/wordpress/
+**/
